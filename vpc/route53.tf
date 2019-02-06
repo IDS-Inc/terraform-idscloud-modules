9096e85be2d093e11714c265bf3dcd66af3fc413
Adrian Herrera <simplyadrian@gmail.com>
2019-02-06 16:23:49 +0000

    adding docs and update to route53 module

--- vpc/route53.tf
+++ vpc/route53.tf
@@ -1,10 +1,12 @@
 resource "aws_route53_zone" "main" {
   name   = "${var.service_dns_zone_name}"
-  vpc_id = "${aws_vpc.main.id}"
+  vpc {
+    vpc_id    = "${aws_vpc.main.id}"
+  }
 }
 
 #resource "aws_service_discovery_private_dns_namespace" "main" {
-#  name   = "${var.service_dns_zone_name}"
-#  vpc_id = "${aws_vpc.main.id}"
+#  name   = "${var.service_dns_zone_name}.local"
+#  vpc    = "${aws_vpc.main.id}"
 #}
 
